9b362ce2571fcdfee33e0bd304319a306331199f
Andrew Wilkins <axwalk@gmail.com>
2015-02-10 11:25:14 +0800

    Add "NotAssigned" error type
    Extracted from juju/juju/state, to be reused
    for volume/storage-instance relationship.

--- errortypes.go
+++ errortypes.go
@@ -208,3 +208,28 @@ func IsNotProvisioned(err error) bool {
 	_, ok := err.(*notProvisioned)
 	return ok
 }
+
+// notAssigned represents an error when something is not yet assigned to
+// something else.
+type notAssigned struct {
+	Err
+}
+
+// NotAssignedf returns an error which satisfies IsNotAssigned().
+func NotAssignedf(format string, args ...interface{}) error {
+	return &notAssigned{wrap(nil, format, " not assigned", args...)}
+}
+
+// NewNotAssigned returns an error which wraps err that satisfies
+// IsNotAssigned().
+func NewNotAssigned(err error, msg string) error {
+	return &notAssigned{wrap(err, msg, "")}
+}
+
+// IsNotAssigned reports whether err was created with NotAssignedf() or
+// NewNotAssigned().
+func IsNotAssigned(err error) bool {
+	err = Cause(err)
+	_, ok := err.(*notAssigned)
+	return ok
+}
